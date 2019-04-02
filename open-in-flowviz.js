7f79c9b4bf188962cf77eb94add6b6204cd458c9
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2019-04-02 11:26:44 +0200

    linter-spot

--- open-in-flowviz.js
+++
@@ -1,4 +0,0 @@
-function open_in_flowviz() {
-  var url = generate_permalink(current_challenge, encode_challenge, "flowviz");
-  window.open(url, '_self');
-};
--- permalinking/scripts.js
+++ permalinking/scripts.js
@@ -37,7 +37,8 @@ function encode_challenge(string_challenge) {
 
   var encoded = encodeURIComponent(string_challenge);
   var sanitized = encoded.replace(/\(/g, '%28').replace(/\)/g, '%29');
-  return sanitized;
+  var de_tabbed = encoded.replace(/%09/g, '%20%20');
+  return de_tabbed;
 };
 
 // sanitization from:  https://github.com/pgbovine/OnlinePythonTutor/blob/0dcdacc7ff5be504dd6a47236ebc69dc0069f991/v5-unity/js/opt-frontend.ts#L62
