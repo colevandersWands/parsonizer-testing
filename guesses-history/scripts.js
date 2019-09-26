416d1322af8d01c237d88cc6b8535986f0886e04
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2019-09-26 09:37:12 +0200

    no-answer-in-review

--- guesses-history/scripts.js
+++ guesses-history/scripts.js
@@ -1,6 +1,6 @@
 function init_modal(new_challenge) {
-  var the_answer = document.getElementById("the-answer");
-  the_answer.innerHTML = new_challenge;
+  // var the_answer = document.getElementById("the-answer");
+  // the_answer.innerHTML = new_challenge;
 
   var the_guesses = document.getElementById("the-guesses");
   while (the_guesses.firstChild) {
@@ -51,36 +51,36 @@ function save_guess() {
 
   var is_correct = parsons_instance.grader.grade().success;
 
-  guesses.push({guess: copy_guess, success: is_correct});
+  guesses.push({ guess: copy_guess, success: is_correct });
 
-/*
-  var indentations = [];
-  for (var i = 0; i < ul_guess.children.length; i++) {
-    var raw_margin = ul_guess.children[i].style.cssText;
-    var margin_num = raw_margin.slice(13, raw_margin.length-3);
-    var num_tabs = margin_num / 50;
-    var tabs = "";
-    while (num_tabs !== 0) {
-      tabs += '\t';
-      num_tabs--;
+  /*
+    var indentations = [];
+    for (var i = 0; i < ul_guess.children.length; i++) {
+      var raw_margin = ul_guess.children[i].style.cssText;
+      var margin_num = raw_margin.slice(13, raw_margin.length-3);
+      var num_tabs = margin_num / 50;
+      var tabs = "";
+      while (num_tabs !== 0) {
+        tabs += '\t';
+        num_tabs--;
+      };
+      indentations.push(tabs);
     };
-    indentations.push(tabs);
-  };
 
-  var lines = [];
-  for (var i = 0; i < ul_guess.children.length; i++) {
-    var code_line = "";  
-    for (var j = 0; j < ul_guess.children[i].length; j++) {
-      code_line += ul_guess.children[i].children[j]
+    var lines = [];
+    for (var i = 0; i < ul_guess.children.length; i++) {
+      var code_line = "";
+      for (var j = 0; j < ul_guess.children[i].length; j++) {
+        code_line += ul_guess.children[i].children[j]
+      };
+      lines.push(code_line);
     };
-    lines.push(code_line);
-  };
 
-  var indented_lines = [];
-  for (var i = 0; i < lines.length; i++) {
-    indented_lines.push(indentations[i] + lines[i]);
-  };
-  
-  var guess = indented_lines.join("\n");
-*/
+    var indented_lines = [];
+    for (var i = 0; i < lines.length; i++) {
+      indented_lines.push(indentations[i] + lines[i]);
+    };
+
+    var guess = indented_lines.join("\n");
+  */
 };
